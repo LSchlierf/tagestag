3c0d9002bca7d4a2f8adb3017283b441a89a1bf0
Lucas Schlierf <lucas.schlierf@tum.de>
2025-07-16 13:25:27 +0200

    made app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
+function getMonth(initial, cur) {
+  if (initial.getDate() === cur.getDate()) {
+    let month = cur.getMonth() - initial.getMonth() + (24 * (cur.getYear() - initial.getYear()))
+
+    return <div className='month'>
+      ({month}. Monatstag)
+    </div>
+  }
+  return <></>
+}
+
+function getYear(initial, cur) {
+  if (initial.getDate() === cur.getDate() && initial.getMonth() === cur.getMonth()) {
+    let year = cur.getYear() - initial.getYear()
+
+    return <div className='year'>
+      {year}. Jahrestag, <b>boa</b>
+    </div>
+  }
+  return <></>
+}
+
 function App() {
+  let initial = new Date("15 Oct 2024")
+  let cur = new Date()
+
+  let tagesTag = Math.floor((cur - initial) / (1000 * 60 * 60 * 24))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='day'>
+        {tagesTag}. Tagestag :)
+      </div>
+      {getMonth(initial, cur)}
+      {getYear(initial, cur)}
     </div>
   );
 }
